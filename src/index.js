21fe9d5d32798617a5dd2c3b98d65a43091840a5
Gangyong92 <hyeonggang@naver.com>
2021-01-02 18:03:12 +0900

    #2.1 Vanilla-JS ~#1.3

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+number.innerText = 0;
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    // 초기화 루틴. ADD, MINUS가 없을 거니까.
+    return count;
+  }
 };
+const countStore = createStore(countModifier);
+
+// 초기화 될때는 subscribe에 연결 전이기 때문에 0은 출력 안될 거임. 그래서 위에
+// 0으로 초기화.
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  count = count + 1;
-  updateText();
+  countStore.dispatch({ type: "ADD" });
 };
 const handleMinus = () => {
-  count = count - 1;
-  updateText();
+  countStore.dispatch({ type: "MINUS" });
 };
 
 add.addEventListener("click", handleAdd);
