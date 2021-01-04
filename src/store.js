0d8d415ca4b521ce3638f726d3d66338d65098b7
Gangyong92 <hyeonggang@naver.com>
2021-01-04 17:14:09 +0900

    #4.1 React-JS ~#3.4
    Home

---
+++ src/store.js
@@ -0,0 +1,41 @@
+import { createStore } from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+const addToDo = (text) => {
+  return {
+    type: ADD,
+    text,
+    id: Date.now(),
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE,
+    id: parseInt(id),
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      const newToDoObj = action;
+      return [newToDoObj, ...state];
+    case DELETE:
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export const actionCreator = {
+  addToDo,
+  deleteToDo,
+};
+
+export default store;
